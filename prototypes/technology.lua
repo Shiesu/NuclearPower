ece834c6cbd7f2ee0282336e351077d5feb4b8d0
Stormseer <denizakak@hotmail.com>
2016-04-01 15:18:29 +0200

    Added technology.lua and the strings associated with it.

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,1 +1,37 @@
--- It's not empty.
+-- Filename: technology.lua
+-- File location: NuclearPower\prototypes\
+
+data:extend(
+{
+	{
+	type = "technology",
+	name = "nuclear-power",
+	icon = "__NuclearPower__/graphics/technology/uranium-processing.png",
+	prerequisites = {"sulfur-processing"}, -- Unsure about this. When should it actually be availible?
+	unit =
+	{ -- Should change costs when we get to balancing.
+		count = 60,
+		ingredients = {
+			{"science-pack-1", 1},
+			{"science-pack-2", 1},
+		},
+		time = 30
+	},
+	order = "d-c", -- Unsure about this. Will have to try the ordering in-game.
+	effects = 
+	{
+		{
+		type = "unlock-recipe",
+		recipe = "processed-uranium"
+		},
+		{
+		type = "unlock-recipe",
+		recipe = "uranium-refinery"
+		},
+		{
+		type = "unlock-recipe",
+		recipe = "nuclear-power-plant"
+		}
+	}
+	}
+})
